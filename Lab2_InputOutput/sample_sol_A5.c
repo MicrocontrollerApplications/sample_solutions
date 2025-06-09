53eda0d3c7f6dbec055bf6b44d8ec438e81c9755
Sebastian Moosbauer <sebastian.moosbauer@thu.de>
2025-06-09 21:33:10 +0200

    Add sample_sol_A5.c

---
+++ Lab2_InputOutput/sample_sol_A5.c
@@ -0,0 +1,75 @@
+/*
+ * File:   newmain.c
+ * Author: Sebastian Moosbauer
+ *
+ * Created on March 19, 2025, 11:24 AM
+ */
+
+
+#include <xc.h>
+
+/**
+ * Initialization function for our program. You'll learn to extend this today :)
+ */
+void __init(void);
+
+int main(void) {
+    // initialize microcontroller
+    __init();
+    
+    
+    
+    // variable to store button's (TL) state
+    unsigned char button;
+    // variable to store the number of times TM is pressed
+    unsigned char button_pressed_counter = 0;
+    while(1){
+        button = PORTBbits.RB1; // read TM's state
+        if(button == 0){ // is TM pressed?
+            while(button == 0){ // do nothing while button is pressed
+                Nop();
+            }
+            button_pressed_counter += 1; // increase button pressed counter
+            if (button_pressed_counter > 15){
+                button_pressed_counter = 0; // reset counter
+            }
+            
+            /* 
+             * ~: bitwise NOT => inverts every bit of the variable
+             * ~ 0b1010 0011 = 0b0101 1100
+            */
+            unsigned char led_mask = ~(button_pressed_counter << 2);
+            // toggle 
+            LATB = led_mask;
+        }
+    }
+    
+//    while(1){
+//        button = PORTBbits.RB2; // Read button TL
+//
+//        if (button == 0) { // TL pressed?
+//            LATBbits.LATB3 = 0; // LED2 on
+//        } else{
+//            LATBbits.LATB3 = 1;  // LED2 off
+//        }
+//
+//        if (PORTBbits.RB4 == 0) { // TR pressed?
+//            while (PORTBbits.RB4 == 0) { // wait until TR is not pressed anymore
+//                Nop();
+//            }
+//
+//            LATBbits.LATB5 = !LATBbits.LATB5; //toggle LED4
+//        }
+//        Nop(); // Do nothing, a breakpoint may be placed here.
+//    }
+    
+    return 0;
+}
+
+void __init(void){
+    OSCCON = 0x50; // 500kHz internal clock (we'll learn that soon!))
+    ANSELB = 0; // set all pins in register B to be digital
+    TRISB = 0b00010100; //Input: RB2 and RB4; Output: all others
+    // LATB = 0b00010100; // Turn LEDs off
+    LATB = 0b00111100; // Turn all LEDs off
+}
